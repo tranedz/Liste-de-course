7afb56735ec0e692106ae54d8a9a94a14635c5ce
tranedz <daulsen@gmail.com>
2022-08-20 00:39:59 +0200

    Changement complet du projet. Initial commit

--- script.js
+++ script.js
@@ -1,239 +1,248 @@
-const form = document.querySelector('#main-form');
-const styleForm = document.querySelector('#style-form');
-const input = document.querySelector('#input-text');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
 const ul = document.querySelector('ul');
+const allBtnDelete = document.querySelectorAll('.delete');
+const btnConfirm = document.querySelector('button');
+const btnDeleteList = document.getElementById('deleteAll');
 
-// KEEP FOCUS ON FIELD
+
+let message = document.createElement('p');
+message.className = 'message';
+document.querySelector('#msg-status').appendChild(message);
+
+let listUser = ["Apprendre JavaScript", "Apprendre HTML", "Apprendre CSS", "Apprendre NodeJS et ExpressJS"];
+
 input.focus();
 
-const clearBtn = document.querySelector('#clear-btn');
+// Submit a new task
+form.addEventListener('submit', (e) => {
+    if (inputSize()) {
+        e.preventDefault();
+        newTask();
+        emptyListMessage();
+        // Empty field after submit
+        input.value = '';
+    } else {
+        alert("Vous n'avez encore rien écrit.");
+    }
 
-// ASSIGN ARRAY TO LOCAL STORAGE
-let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-localStorage.setItem('items', JSON.stringify(itemsArray));
-
-// ARRAY FOR DATE
-const data = JSON.parse(localStorage.getItem('items'));
-
-// console.log(itemsArray);
-// console.log(data);
-
-// STYLE APPAREANCE
-let arrayColors = localStorage.getItem('colors') ? JSON.parse(localStorage.getItem('colors')) : [];
-localStorage.setItem('colors', JSON.stringify(arrayColors));
-
+    // Réactiver le bouton effacer liste
+    btnDeleteList.disabled = false;
+    btnDeleteList.style.backgroundColor = "red";
+})
 
 
 function inputSize() {
-    if(input.value.length > 0) {
+    if (input.value.length > 0) {
         return true;
-    }
-    return false;
-};
-
-
-// CREATE NEW ITEM
-function createItem(text) {
-
-        const item = document.createElement('li');
-
-        const editBtn = createEditBtn();
-
-        // CREATE CHILD EDIT BUTTON
-        function createEditBtn() {
-            // <span class="material-symbols-outlined edit-btn">edit</span>
-            const editBtn = document.createElement('span');
-            editBtn.setAttribute('class', 'material-symbols-outlined edit-btn');
-            editBtn.appendChild(document.createTextNode('edit'));
-            return editBtn;
-        };
-
-        // CREATE CHILD SPAN TEXT CONTENT
-        // <span class="item-name li-checked">Acheter des fruits</span>
-        const itemName = document.createElement('span');
-        itemName.setAttribute('class', 'item-name');
-        itemName.textContent = text;
-
-        // CREATE INPUT'S ITEM FOR UPDATING SPAN TEXT CONTENT
-        const itemInput = document.createElement('input');
-        itemInput.setAttribute('type', 'text');
-        itemInput.className = 'item-input';
-        itemInput.classList.add('invisible');
-        itemInput.value = itemName.textContent;
-
-        // CREATE EDIT OFF BUTTON
-        const closeEditBtn = createCloseEditBtn();
-        function createCloseEditBtn() {
-            // < span class="material-symbols-outlined invisible" >edit_off</span >
-            const closeEditBtn = document.createElement('span');
-            closeEditBtn.setAttribute('class', 'material-symbols-outlined close-edit-btn');
-            closeEditBtn.appendChild(document.createTextNode('edit_off'));
-            closeEditBtn.style.display = 'none';
-            return closeEditBtn;
-        };
-
-        // EVENT FOR EDIT OFF BUTTON
-        // DISPLAY INPUT'S ITEM
-        editBtn.addEventListener('click', displayItemInput);
-        function displayItemInput(e) {
-            // Faire disparaître le bouton d'édition et faire apparaître close edition
-            e.target.style.display = 'none';
-            closeEditBtn.style.display = 'block';
-            // Faire disparaître le span contenant le nom de l'article
-            itemName.classList.add('invisible');
-            // Faire apparaître l'input pour l'édition de l'article
-            itemInput.classList.remove('invisible');
-            itemInput.focus();
-        };
-
-        // CREATE CLOSE BUTTON WHO DELETE ITEM
-        // <span class="material-symbols-outlined close-btn">close</span>
-        const closeBtn = document.createElement('span');
-        closeBtn.setAttribute('class', 'material-symbols-outlined close-btn');
-        closeBtn.appendChild(document.createTextNode('close'));
-
-        // EVENT FOR CLOSE EDIT BUTTON
-        // GETTING THE NEW VALUE FOR SPAN TEXT CONTENT
-        closeEditBtn.addEventListener('click', closeItemInput);
-        function closeItemInput(e) {
-            e.target.style.display = 'none';
-            editBtn.style.display = 'block';
-            itemName.classList.remove('invisible');
-            itemInput.classList.add('invisible');
-            
-            let newValue = itemInput.value;
-            // UPDATE LOCAL STORAGE ELEMENT
-            updateOnStorage(itemName.textContent, newValue);
-            itemName.textContent = newValue;
-
-            if (itemName.textContent !== '') {
-                console.log(itemName.textContent);
-            } else {
-                alert('vide');
-            }
-            // console.log('New value :', newValue);
-        }
-
-        // APPEN CHILD ITEM AND PARENT UL
-        item.appendChild(editBtn);
-        item.appendChild(closeEditBtn);
-        item.appendChild(itemName);
-        item.appendChild(itemInput);
-
-        // INSERT THE NEW ITEM ON TOP
-        item.insertBefore(itemInput, itemName);
-
-        item.appendChild(closeBtn);
-
-        ul.appendChild(item);
-        ul.insertBefore(item, ul.firstElementChild);
-
-        // VALIDATE ITEM
-        item.addEventListener('click', (e) => {
-            if (e.target.classList.contains('item-name')) {
-                return e.target.classList.toggle('li-checked');
-            };
-        });
-        return item;
-};
-
-// SUBMIT VALUE
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    let inputValue = input.value;
-
-    if (inputSize()) {
-        // ADD SPAN TEXT CONTENT TO AN ARRAY
-        itemsArray.push(inputValue);
-        // ADD SPAN TEXT CONTENT TO LOCAL STORAGE
-        localStorage.setItem('items', JSON.stringify(itemsArray));
-        createItem(inputValue);
     } else {
-        alert(`Vous devez d'abord écrire quelque chose.`);
-    }
-
-    // EMPTY THE FIELD
-    input.value = "";
-});
-
-// RESTORING DATA FROM LOCAL STORAGE
-// CREATING EQUIVALENT ITEM
-data.forEach(userItem => {
-    createItem(userItem);
-    // ul.addEventListener('click', deleteItem);
-});
-
-// CLEAR LOCAL STORAGE AND ALL ITEMS
-clearBtn.addEventListener('click', function () {
-
-    if (confirm("Cette action effacera toute la liste. Souhaitez-vous continuer ?")) {
-        localStorage.clear();
-        itemsArray = [];
-        while (ul.firstChild) {
-            ul.removeChild(ul.firstChild);
-        }
-    }
-});
-
-// DELETE AN ITEM FROM
-ul.addEventListener('click', deleteItem);
-
-function deleteItem(e) {
-    let isCloseBtn = e.target.classList.contains('close-btn');
-    if (isCloseBtn) {
-        e.target.parentNode.remove();
-        // console.log(e.target.parentElement.childNodes[3]);
-
-        // DELETE FROM LOCAL STORAGE
-        removeFromStorage(e.target.parentElement.childNodes[3]);
-    }
-};
-
-// FN DELETE FROM LOCAL STORAGE
-function removeFromStorage(liItem) {
-    // console.log(liItem);
-    let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-    //  console.log(itemsArray);
-
-    itemsArray.forEach(function (singleElement, index) {
-
-        // console.log(singleElement, index);
-
-        if (liItem.textContent === singleElement) {
-
-            itemsArray.splice(index, 1);
-        }
-    });
-    // console.log(itemsArray);
-    localStorage.setItem('items', JSON.stringify(itemsArray));
-};
-
-// FN UPDATE ELEMENT ON LOCAL STORAGE
-function updateOnStorage(precendItem, itemUpdated) {
-    let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-    // console.log('verif ', itemsArray);
-    itemsArray.forEach((singleElement, index) => {
-        if (precendItem === singleElement) {
-            // REPLACE OLD BY NEW ELEMENT
-            itemsArray.splice(index, 1, itemUpdated);
-            // arr.splice(index, 1, item);
-        }
-    });
-
-    localStorage.setItem('items', JSON.stringify(itemsArray));
-    // console.log(itemsArray);
-}
-
-// BUG
-// MESSAGE IF LIST IS EMPTY
-function isEmpty() {
-    if (ul.childElementCount <= 0) {
-        const para = document.createElement('p');
-        para.textContent = 'Votre liste est vide.';
-
-        document.querySelector('#message-container').appendChild(para);
-        document.querySelector('.clear-btn-container').style.visibility = 'hidden';
+        return false;
     }
 }
-isEmpty();
+
+function createTasks() {
+    for (let item of listUser) {
+        task = document.createElement('li');
+        task.textContent = item;
+        ul.appendChild(task);
+
+
+        const btnUpdate = document.createElement('span');
+        btnUpdate.textContent = "Modifier";
+        btnUpdate.classList = "edit";
+
+        const btnDelete = document.createElement('span');
+        btnDelete.textContent = "Supprimer";
+        btnDelete.classList = "delete";
+
+        task.appendChild(btnUpdate);
+        task.appendChild(btnDelete);
+
+        btnDelete.addEventListener('click', deleteTask);
+    }
+}
+
+
+
+createTasks();
+
+// Add new task
+function newTask() {
+    let userValue = input.value;
+    const task = document.createElement('li');
+    task.textContent = userValue;
+    // task.classList = "task";
+    ul.appendChild(task);
+
+    const btnUpdate = document.createElement('span');
+    btnUpdate.textContent = "Modifier";
+    btnUpdate.classList = "edit";
+
+    const btnDelete = document.createElement('span');
+    btnDelete.textContent = "Supprimer";
+    btnDelete.classList = "delete";
+
+    task.appendChild(btnUpdate);
+    task.appendChild(btnDelete);
+
+    btnDelete.addEventListener('click', deleteTask);
+
+    btnUpdate.addEventListener('click', updateTask);
+
+}
+
+// Get all btn for deleting / only existing tasks
+for (let i = 0; i < allBtnDelete.length; i++) {
+    const btnDelete = allBtnDelete[i];
+    btnDelete.addEventListener('click', deleteTask);
+    // console.log(btnDelete)
+}
+
+// Delete a task of the list
+function deleteTask(e) {
+    let taskName = e.target.parentElement.firstChild.textContent;
+    deletingMessage(taskName);
+
+    setTimeout(() => {
+        e.target.parentElement.remove();
+
+    }, 3000);
+
+    setTimeout(() => {
+        emptyListMessage()
+    }, 3000);
+}
+
+
+function deletingMessage(content) {
+
+    // Loading simulation
+    setTimeout(() => {
+        message.textContent = `Suppression de : ${content}.`;
+    }, 500)
+    setTimeout(() => {
+        message.textContent = `Suppression de : ${content}..`;
+    }, 1000)
+    setTimeout(() => {
+        message.textContent = `Suppression de : ${content}...`;
+    }, 1500)
+
+    // Loading
+    setTimeout(() => {
+        message.textContent = `Suppression de : ${content}.`;
+    }, 2000)
+    setTimeout(() => {
+        message.textContent = `Suppression de : ${content}..`;
+    }, 2500)
+    setTimeout(() => {
+        message.textContent = `Suppression de : ${content}...`;
+    }, 3000)
+
+    setTimeout(() => {
+        message.textContent = '';
+    }, 3000);
+}
+
+
+function emptyListMessage() {
+    if (ul.childElementCount === 0) {
+        message.textContent = "Votre liste est vide. Besoin d'idées ?";
+        btnDeleteList.disabled = true;
+        btnDeleteList.style.backgroundColor = "grey";
+    } else {
+        message.textContent = '';
+    }
+}
+
+
+emptyListMessage();
+
+
+const allBtnUpdate = document.querySelectorAll('.edit');
+// Update tasks
+// Get all btn for deleting / only existing tasks
+for (let i = 0; i < allBtnUpdate.length; i++) {
+    const btnUpdate = allBtnUpdate[i];
+    btnUpdate.addEventListener('click', updateTask);
+}
+
+// Mise à jour
+function updateTask(e) {
+    // Récupérer l'élément à mettre à jour
+    let taskToUpdate = e.target.parentElement.firstChild.textContent;
+    // Récupérer l'ancien élément à remplacer plus tard
+    const oldItem = e.target.parentElement;
+
+    // Récupéer l'élément ul parent
+    const taskElement = e.target.parentElement.parentElement;
+
+    // Création d'un formulaire avec input et bouton à l'intérieur d'un élément li
+    // li
+    const newItem = document.createElement('li');
+    // Form
+    const formForUpdate = document.createElement('form');
+    // Input
+    const inputForUpdate = document.createElement('input');
+    // Bouton
+    const btnForUpdate = document.createElement('button');
+    // Afficher la valeur de l'ancien élément dans le nouvel input
+    inputForUpdate.value = taskToUpdate;
+
+    btnForUpdate.classList = "update";
+    btnForUpdate.textContent = "Mettre à jour";
+
+    formForUpdate.appendChild(inputForUpdate);
+    formForUpdate.appendChild(btnForUpdate);
+
+    newItem.appendChild(formForUpdate);
+
+    // Remplacer l'ancien élément par le nouveau
+    taskElement.replaceChild(newItem, oldItem);
+
+    // Action pour mettre à jour un élément
+    formForUpdate.addEventListener('submit', (e) => {
+        e.preventDefault();
+        // Remplacer le nouveau par l'ancien élément
+        taskElement.replaceChild(oldItem, newItem);
+        // Et donner la valeur du nouveau à l'ancien
+        oldItem.firstChild.textContent = inputForUpdate.value;
+
+        message.innerHTML = `Une tâche mise à jour : <br>
+             - ancien : "${taskToUpdate}", <br>
+            - nouveau : "${oldItem.firstChild.textContent}".`;
+
+        setTimeout(() => {
+            message.textContent = '';
+        }, 5000);
+    })
+}
+
+
+
+const listItem = ul.children;
+const listItemPre = document.querySelectorAll('li');
+
+btnDeleteList.addEventListener('click', deleteAllList)
+
+function deleteAllList(e) {
+
+    if (confirm('Cette action effacera toute la liste. Continuez ?') == true) {
+        deletingMessage("toute la liste");
+
+        setTimeout(() => {
+            for (const itemPre of listItemPre) {
+                itemPre.remove();
+            }
+
+            for (let i = 0; i < listItem.length; i++) {
+                const item = listItem[i];
+                item.remove();
+            }
+            emptyListMessage();
+        }, 3000)
+
+        e.target.disabled = true;
+        e.target.style.backgroundColor = "grey";
+    }
+}
+
